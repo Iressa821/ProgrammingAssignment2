60d088bc2012fedf3fef624dbbf456ce88e0b695
Iressa821 <65990832+Iressa821@users.noreply.github.com>
2020-07-14 20:04:05 -0400

    Update cachematrix.R
    7.14

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function will create two objects x and i, then define these two objects, and eventually create a list
+## containing x and i as well as four functions,
 
 makeCacheMatrix <- function(x = matrix()) {
-
+          i <- NULL
+          set <- function(y){
+                    y <<- x
+                    i <<- NULL
+          }
+          get <- function() x
+          setinverse <- function(inverse) i <<- inverse 
+          getinverse <- function() i
+          list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function will first check if the inverse has been defined or not. If yes, return the defined inverse; If
+## not, compute the inverse then print it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          ## Return a matrix that is the inverse of 'x'
+          i <- x$getinverse()
+          if(!is.null(i)){
+                    message("getting cached data")
+                    return(i)
+          }
+          data <- x$get()
+          i <- solve(data, ...)
+          x$setinverse(i)
+          i
 }
+
+## Thanks for reviewing my assignment
